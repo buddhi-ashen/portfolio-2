2528ec23dad90c01e6bf044d4687e4f120ae8149
buddhi_ict <buddhiict@gmail.com>
2022-06-08 20:32:07 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,48 @@
+//!TOGGLE NAVIGATION MENU AND ICONS
+const menuToggleButton = document.querySelector('.menu-toggle-button');
+const menuElement = document.querySelector('.menu');
+
+const toggleMenu = () => {
+    menuElement.classList.toggle('active');
+    menuToggleButton.classList.toggle('active')
+};
+
+menuToggleButton.addEventListener('click', toggleMenu);
+// !REMOVE ACTIVE CLASS FROM AND ICON ON LINK CLICK
+const removeActiveLinkClass = e => {
+    if(e.target.classList.contains('list-link')){
+        menuElement.classList.remove('active');
+        menuToggleButton.classList.remove('active');
+    }
+}
+
+document.addEventListener('click', removeActiveLinkClass);
+
+// !TOGGLE THEME AND STORE SELECTION WITHIN LOCAL STORAGE
+const themetoggleButton = document.querySelector('.theme-toggle-button');
+const bodyElement = document.body;
+const currentTheme = localStorage.getItem('darkTheme');
+
+if(currentTheme){
+    bodyElement.classList.add('dark-theme');
+}
+
+const toggleTheme = () => {
+    bodyElement.classList.toggle('dark-theme');
+
+    if(bodyElement.classList.contains('dark-theme')){
+        localStorage.setItem('darkTheme', 'active');
+    }else{
+        localStorage.removeItem('darkTheme');
+    }
+};
+
+themetoggleButton.addEventListener('click', toggleTheme);
+// !SCROLL REVEAL
+const sr = ScrollReveal ({
+    distance:'50px',
+    duration: 1500,
+    easing: 'cubic-bezier(0.68, -0.55. 0.265, 1.55'
+});
+
+sr.reveal('.main-title', {origin: 'top'});
